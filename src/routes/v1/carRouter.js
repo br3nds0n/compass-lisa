9ae5db48d230643a0d3944514b7056469a5296c6
br3nds0n <brendson.net@gmail.com>
2022-01-24 22:20:26 -0300

    feat: feat/0f56546/create-post-cars-route-validation

--- src/routes/v1/carRouter.js
+++ src/routes/v1/carRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 
 const CarController = require('../../app/controller/carController')
+const validationCreate = require('../../app/validation/car/create')
 
 router
-  .post('/car', CarController.create)
+  .post('/car', validationCreate, CarController.create)
 
 module.exports = router
