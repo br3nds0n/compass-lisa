5059dbbca3c8b9fa4afd6859ec33590882423a07
br3nds0n <brendson.net@gmail.com>
2022-01-27 16:45:57 -0300

    ref: refactor-validation-car

--- src/routes/v1/carRouter.js
+++ src/routes/v1/carRouter.js
@@ -3,15 +3,14 @@ const router = require('express').Router()
 const carController = require('../../app/controller/carController')
 const CarController = require('../../app/controller/carController')
 
-const validationCreate = require('../../app/validation/car/body/create')
-const validationUpdate = require('../../app/validation/car/body/update')
+const validationBodyCar = require('../../app/validation/car/body/validationBodyCar')
 const validationFind = require('../../app/validation/car/query/findAll')
 const validationId = require('../../app/validation/car/query/validationId')
 
 router
-  .post('/car', validationCreate, CarController.create)
+  .post('/car', validationBodyCar, CarController.create)
   .get('/car', validationFind, CarController.findAll)
   .delete('/car/:id', validationId, CarController.delete)
-  .put('/car/:id', validationId, validationUpdate, CarController.update)
+  .put('/car/:id', validationId, validationBodyCar, CarController.update)
   .get('/car/:id', validationId, carController.getById)
 module.exports = router
