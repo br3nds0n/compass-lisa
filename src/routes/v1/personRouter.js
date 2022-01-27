64eb53c55f5c6a58468bf77cf4e1b9f894f33104
br3nds0n <brendson.net@gmail.com>
2022-01-27 19:01:55 -0300

    feat: create-route-pick-up-person

--- src/routes/v1/personRouter.js
+++ src/routes/v1/personRouter.js
@@ -5,11 +5,13 @@ const PersonController = require('../../app/controller/personController')
 const validationBodyPerson = require('../../app/validation/person/body/validationBodyPerson')
 const validationUpdate = require('../../app/validation/person/body/update')
 const validationId = require('../../app/validation/validationId')
+const validationQuery = require('../../app/validation/person/query/findAll')
 
 router
   .post('/people', validationBodyPerson, PersonController.create)
-  .get('/people', PersonController.findAll)
+  .get('/people', validationQuery, PersonController.findAll)
   .delete('/people/:id', validationId, PersonController.delete)
   .put('/people/:id', validationId, validationUpdate, PersonController.update)
+  .get('/people/:id', validationId, PersonController.getById)
 
 module.exports = router
