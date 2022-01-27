adfbf5f6dac062ad53240daa033b9b0d5a68be97
br3nds0n <brendson.net@gmail.com>
2022-01-27 00:55:47 -0300

    ref: authentication refactoring

--- src/app/controller/authentication/authenticateController.js
+++ src/app/controller/authentication/authenticateController.js
@@ -1,14 +1,27 @@
 const schema = require('../../schema/personSchema')
+const bcrypt = require('bcryptjs')
+
+const generateToken = require('../../helper/generateToken')
 
 class AuthenticateController {
   async authenticate (req, res) {
     const { email, senha } = req.body
+    const user = await schema.findOne({ email }).select('+senha')
 
-    const user = await schema.findOne({ email, senha })
+    if (!user) {
+      return res.status(400).json({ error: 'user not found' })
+    }
 
-    if (!user) { return res.status(400).json({ error: 'not found' }) }
+    if (!await bcrypt.compare(senha, user.senha)) {
+      return res.status(400).json({ error: 'Invalid password' })
+    }
 
-    res.send({ user })
+    user.senha = undefined
+
+    res.send({
+      user,
+      token: generateToken({ id: user.id })
+    })
   }
 }
 
