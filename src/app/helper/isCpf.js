a148c421878c060c74b60e50ed32e8db08176a1f
br3nds0n <brendson.net@gmail.com>
2022-01-29 00:21:22 -0300

    ref: refactor-iscpf-file

--- src/app/helper/isCpf.js
+++ src/app/helper/isCpf.js
@@ -2,7 +2,20 @@ function isValid (cpf) {
   let addition = 0
   let rest
 
-  if (cpf === '00000000000' && cpf.length !== 11) return false
+  if (!cpf ||
+    cpf.length !== 11 ||
+    cpf === '00000000000' ||
+    cpf === '11111111111' ||
+    cpf === '22222222222' ||
+    cpf === '33333333333' ||
+    cpf === '44444444444' ||
+    cpf === '55555555555' ||
+    cpf === '66666666666' ||
+    cpf === '77777777777' ||
+    cpf === '88888888888' ||
+    cpf === '99999999999') {
+    return false
+  }
 
   for (let i = 1; i <= 9; i++) addition = addition + parseInt(cpf.substring(i - 1, i)) * (11 - i)
   rest = (addition * 10) % 11
