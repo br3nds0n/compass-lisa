26479297a46acf6f051f36fe28db614d72832b5c
Brendson <brendson.net@gmail.com>
2022-02-02 14:47:26 -0300

    Revert "Merge"

--- src/app/helper/isYear18.js
+++
@@ -1,7 +0,0 @@
-function isYear18 (dateOfBirth) {
-	const date18YrsAgo = new Date();
-	date18YrsAgo.setFullYear(date18YrsAgo.getFullYear() - 18);
-
-	return dateOfBirth <= date18YrsAgo;
-}
-module.exports = isYear18;
--- src/app/service/AuthService.js
+++
@@ -1,9 +0,0 @@
-const AuthRepository = require('../repository/AuthRepository.js');
-
-class AuthService{  
-	async findAuth(payload){
-		return await AuthRepository.findAuth(payload);
-	}
-}
-
-module.exports = new AuthService();
