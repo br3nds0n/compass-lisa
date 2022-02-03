1238403e360fd99df6fbdb6520ece8d9a1262c74
brendson <brendson.ne@gmail.com>
2022-02-03 18:05:36 -0300

    ref: project

--- src/app.js
+++ src/app.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-
-const routes = require('./routes');
-require('./infra/database/mongo');
-const erroModify = require('./app/error/erroModify');
-
-class App {
-	constructor() {
-		this.server = express();
-		this.middlewares();
-		this.routes();
-		this.erroModify();
-	}
-
-	routes() {
-		this.server.use('/api/', routes);
-	}
-
-	middlewares() {
-		this.server.use(express.urlencoded({ extended: true }));
-		this.server.use(express.json());
-	}
-
-	erroModify() {
-		this.server.use(erroModify);
-	}
-}
-
-module.exports = new App().server;
-=======
 const express = require('express');
 
 const routes = require('./routes');
@@ -60,4 +29,3 @@ class App {
 }
 
 module.exports = new App().server;
->>>>>>> 257b521b1b6ed4b76a410ea6c166959688568964
--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -15,21 +14,3 @@ class Database {
 }
 
 module.exports = new Database().connect();
-=======
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-class Database {
-	constructor () {
-		this.connect();
-	}
-
-	connect () {
-		return mongoose
-			.connect(process.env.DB_HOST)
-			.catch((error) => console.log(error));
-	}
-}
-
-module.exports = new Database().connect();
->>>>>>> 257b521b1b6ed4b76a410ea6c166959688568964
