fbbb64c9796c31e3e37be1a94115d74bf495ec6f
brendson <brendson.ne@gmail.com>
2022-02-06 11:14:14 -0300

    ref: refactor-tests

---
+++ __tests__/database/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const routes = require('../../src/routes');
+require('./connection');
+
+class App {
+	constructor() {
+		this.server = express();
+		this.middlewares();
+		this.routes();
+	}
+
+	routes() {
+		this.server.use('/api/', routes);
+	}
+
+	middlewares() {
+		this.server.use(express.urlencoded({ extended: true }));
+		this.server.use(express.json());
+	}
+
+}
+
+module.exports = new App().server;
