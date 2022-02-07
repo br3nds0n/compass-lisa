82c5635fafbcab50fecf1a4f80c30644ba68160d
brendson <brendson.ne@gmail.com>
2022-02-07 15:55:58 -0300

    ref: ref routes

--- src/routes/v1/authenticateRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const AuthenticateController = require('../../app/controller/authentication/AuthenticateController');
-
-const authenticateValidationBody = require('../../app/validation/authentication/authenticaionValidatebody');
-
-router
-	.post('/authenticate', authenticateValidationBody, AuthenticateController.authenticate);
-
-module.exports = router;
