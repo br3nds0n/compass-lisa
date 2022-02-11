f3147f9768d30d66b2dc2581159e7d88f4874cb0
brendson <brendson.ne@gmail.com>
2022-02-11 10:20:03 -0300

    feat: create-route-swagger

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,7 +4,8 @@ const carRouter = require('./components/carRouter');
 const person = require('./components/personRouter');
 const authenticate = require('./components/authenticateRouter');
 const rentalRouter = require('./components/rentalRouter');
+const swaggerRouter = require('./components/swaggerRouter');
 
-router.use(carRouter, person, authenticate, rentalRouter);
+router.use(carRouter, person, authenticate, rentalRouter, swaggerRouter);
 
 module.exports = router;
