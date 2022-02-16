4dd763f40313e8394c8565669c6e5d4b3faa9274
brendson <brendson.ne@gmail.com>
2022-02-16 18:19:46 -0300

    ref: middlewares

--- src/app/middlewares/erroModify.js
+++ src/app/middlewares/erroModify.js
@@ -1,11 +1,12 @@
 const HttpError = require('../error/http/httpError');
 
-module.exports = function (err, req, res) {
+module.exports = function (err, req, res, next) {
   if (err instanceof HttpError) {
     res.status(err.statusCode).json({
       message: err.name,
       ...err.body
     });
+    next();
   } else {
     res.status(500).json({
       message: 'Internal Server Error'
