3cd9c480dc14e2b3981b70b2c5571cbbbaacab2f
brendson <brendson.ne@gmail.com>
2022-02-17 01:20:49 -0300

    feat: UnauthorizedError

--- src/app/middlewares/tokenBearer.js
+++ src/app/middlewares/tokenBearer.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
 const auth = require('../config/authenticate.json');
 
+const Unauthorized = require('../error/http/Unauthorized');
+
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decode = jwt.verify(token, auth.secret);
-    req.client = decode;
+    req.people = decode;
     return next();
   } catch (error) {
-    return res.status(401).send({ description: 'Unauthorized', message: 'Invalid login token' });
+    throw new Unauthorized(error.name);
   }
 };
