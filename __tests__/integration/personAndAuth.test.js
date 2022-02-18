5ab773af76a238c3e1af3df25dd49e924a22bfd4
brendson <brendson.ne@gmail.com>
2022-02-17 23:04:29 -0300

    ref: integration-test

--- __tests__/integration/personAndAuth.test.js
+++ __tests__/integration/personAndAuth.test.js
@@ -2,24 +2,24 @@ const supertest = require('supertest');
 const mongoose = require('mongoose');
 
 const App = require('../database/index');
-const personService = require('../../src/app/service/personService');
+const PersonService = require('../../src/app/service/PersonService');
 
 const person = {};
 
 describe('Test-Feature-Person', () => {
   beforeAll(async () => {
-    person.p1 = await personService.create({
+    person.p1 = await PersonService.create({
       nome: 'gabriel',
-      cpf: '81255748993',
+      cpf: '92860405046',
       data_nascimento: '23/04/2003',
       email: 'gabriel@exemple.com',
       senha: '123456',
       habilitado: 'sim'
     });
 
-    person.p2 = await personService.create({
+    person.p2 = await PersonService.create({
       nome: 'julia',
-      cpf: '45571193406',
+      cpf: '05474941064',
       data_nascimento: '23/04/2003',
       email: 'julia@exemple.com',
       senha: '123456',
@@ -28,15 +28,28 @@ describe('Test-Feature-Person', () => {
   });
 
   afterAll(async () => {
-    await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
   });
+
+  /* 
+    GET - PEOPLE
+  */
 
   it('GET /api/v1/people - find All', async () => {
     const res = await supertest(App).get('/api/v1/people');
 
     expect(res.statusCode).toBe(200);
   });
+
+  it('GET /api/v1/people - INVALID', async () => {
+    const res = await supertest(App).get('/api/v1/peop');
+
+    expect(res.statusCode).toBe(404);
+  });
+
+  /* 
+    GET/:id - PEOPLE
+  */
 
   it('GET/:id /api/v1/people/:id', async () => {
     const res = await supertest(App).get(`/api/v1/people/${person.p1._id}`);
@@ -45,10 +58,20 @@ describe('Test-Feature-Person', () => {
     expect(res.body.nome).toBe('gabriel');
   });
 
+  it('GET/:id /api/v1/people/:id - INVALID', async () => {
+    const res = await supertest(App).get(`/api/v1/people/620ebf227ce3f1fc44f00920`);
+
+    expect(res.statusCode).toBe(404);
+  });
+
+  /* 
+    POST - PEOPLE
+  */
+
   it('POST /api/v1/people', async () => {
     const res = await supertest(App).post('/api/v1/people').send({
       nome: 'bredson',
-      cpf: '71255973439',
+      cpf: '57042360060',
       data_nascimento: '23/04/2003',
       email: 'brendson@exemple.com',
       senha: '123456',
@@ -58,10 +81,27 @@ describe('Test-Feature-Person', () => {
     expect(res.statusCode).toBe(201);
   });
 
-  it('PUT /api/v1/people/:id', async () => {
+  it('POST /api/v1/people - INVALID', async () => {
+    const res = await supertest(App).post('/api/v1/people').send({
+      nome: '',
+      cpf: '',
+      data_nascimento: '',
+      email: '',
+      senha: '',
+      habilitado: ''
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  /* 
+    PUT - PEOPLE
+  */
+
+  it('PUT /api/v1/people/:id ', async () => {
     const res = await supertest(App).put(`/api/v1/people/${person.p1._id}`).send({
       nome: 'vinicius',
-      cpf: '81255748993',
+      cpf: '77606493049',
       data_nascimento: '23/04/2003',
       email: 'gabriel@exemple.com',
       senha: '123456',
@@ -71,10 +111,44 @@ describe('Test-Feature-Person', () => {
     expect(res.body.nome).toBe('vinicius');
   });
 
+  it('PUT /api/v1/people/:id - INVALD NOT-FOUND', async () => {
+    const res = await supertest(App).put(`/api/v1/people/620ebf227ce3f1fc44f00920`).send({
+      nome: 'vinicius',
+      cpf: '77606493049',
+      data_nascimento: '23/04/2003',
+      email: 'gabriel@exemple.com',
+      senha: '123456',
+      habilitado: 'sim'
+    });
+    expect(res.statusCode).toBe(404);
+  });
+
+  it('PUT /api/v1/people/:id INVALD BAD-REQUEST', async () => {
+    const res = await supertest(App).put(`/api/v1/people/${person.p1._id}`).send({
+      nome: '',
+      cpf: '',
+      data_nascimento: '',
+      email: '',
+      senha: '',
+      habilitado: ''
+    });
+    expect(res.statusCode).toBe(400);
+  });
+
+  /* 
+    DELETE - PEOPLE
+  */
+
   it('DELETE /api/v1/people/:id', async () => {
     const res = await supertest(App).delete(`/api/v1/people/${person.p1._id}`);
 
     expect(res.statusCode).toBe(204);
+  });
+
+  it('DELETE /api/v1/people/:id - INVALID', async () => {
+    const res = await supertest(App).delete(`/api/v1/people/620ebf227ce3f1fc44f00920`);
+
+    expect(res.statusCode).toBe(404);
   });
 
   /* 
